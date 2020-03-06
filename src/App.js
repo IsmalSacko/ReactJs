0d583dcb2af66bfd1662f4b2f7690a0d29b81843
Ismaila Sacko <ismalsacko@yahoo.fr>
2020-03-06 06:01:51 +0100

    Ajout de nouvelles fonctinalités (boutton pour modifier le le nom et l'âge !

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Membre from './components/Membre'
+import Button from './components/Button'
 const famile ={
   membre1: {nom: 'Ismael', age: 29},
   membre2: {nom: 'Gaye', age:27},
@@ -13,13 +14,35 @@ const famile ={
 class App extends Component{
   state = {famile}
 
-  handleClick = () =>{
+  handleClick = (num) =>{
     // on copie d'abord l'objet à modifier
-    const famile = { ... this.state.famile }
+    const modiAge = { ... this.state.famile }
     // on définit le nbre à incrémentter 
-    famile.membre1.age +=1
-    // Et le passe en passe en paramètre
-    this.setState({famile})
+    modiAge.membre1.age +=num
+    // Et le passe en paramètre
+    this.setState({modiAge})
+
+  }
+
+  handleChange = (event) =>{
+    // on copie d'abord l'objet à modifier
+    const modinom = { ... this.state.famile }
+    // on définit le nbre à incrémentter 
+    const nom = event.target.value
+    modinom.membre1.nom=nom
+    // Et le passe en paramètre
+    this.setState({modinom})
+
+  }
+
+  handleChangeAge = (event) =>{
+    // on copie d'abord l'objet à modifier
+    const modifAge = { ... this.state.famile }
+    // on définit le nbre à incrémentter 
+    const age = event.target.value
+    modifAge.membre1.age=age
+    // Et le passe en paramètre
+    this.setState({modifAge})
 
   }
 
@@ -29,6 +52,8 @@ render () {
    return (
     <div className="App">
      <h1>{titre}</h1>
+     <input type="text" value={famile.membre1.nom} onChange={this.handleChange} />
+     <input type="text" value={famile.membre1.age} onChange={this.handleChangeAge}/>
      <Membre nom= {famile.membre1.nom} age ={famile.membre1.age}/>   
      <Membre nom= {famile.membre2.nom} age ={famile.membre2.age}/>   
      <Membre nom={famile.membre3.nom} age ={famile.membre3.age}/>   
@@ -38,7 +63,7 @@ render () {
          
      je suis un élément fils de la division.
      </Membre>
-     <button onClick={this.handleClick}>Vieillir</button>
+     <Button vieillir={()=>this.handleClick(5)}/>
     </div>
   )
   
@@ -50,3 +75,4 @@ render () {
 
 
 export default App;
+ 
